b438a16c2bc62e52f87f4c0cf1d2badc801ee822
byrontung <btung1@ualberta.ca>
2022-04-20 20:39:42 -0600

    added picture

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./About.css";
+import profile from "../../img/profile.jpg";
+// import BottomNagivationBar from "../bottomNavigationbar/BottomNagivationBar";
 
 function About() {
     return (
         <div className="row page">
             <div className="backgroundColour left">
-                <h1>About</h1>
+                <h1 className="title">About</h1>
+                {/* <BottomNagivationBar /> */}
             </div>
             <div className="backgroundColour right">
-                <h1 className="mainText">Hello! My name is Byron Tung</h1>
+                <div className="profile">
+                    <img className="profileImage" src={profile} alt="Profile" />
+                </div>
                 <h1 className="mainText">Welcome to my website!</h1>
                 <h1 className="mainText">
-                    I am a student at the University of Alberta pursing a
-                    Specialized Computer Science Degree.
+                    Hello! My name is Byron Tung and I am student at the
+                    University of Alberta pursing a Specialized Computer Science
+                    Degree.
                 </h1>
                 <h1 className="mainText">
                     In my spare time, I play and coach badminton. I have played
-                    it for 12+ years and enjoy giving back to the communities
-                    that want my help. Since I was once in their shoes, I
+                    badminton for 12+ years and enjoy giving back to the community.
+                    
+
+
+                    Since I was once in their shoes, I
                     believe that having a mentor and a friend working towards a
                     common goal is a special connection that can be build though
                     sports.
