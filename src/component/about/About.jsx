0929014fe198e1543f930346e3f382c1df29a297
byrontung <btung1@ualberta.ca>
2022-04-23 05:26:08 -0600

    cleaned up comments

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -57,33 +57,6 @@ function About() {
                 </h1>
             </div>
         </div>
-        // {/* <div className="main">
-        //         <BottomNagivationBar />
-        //     </div> */}
-        // {/* <div className="backgroundColour right">
-        //     <div className="profile">
-        //         <img className="profileImage" src={profile} alt="Profile" />
-        //     </div>
-        //     <h1 className="mainText">Welcome to my website!</h1>
-        //     <h1 className="mainText">
-        //         Hello! My name is Byron Tung and I am student at the
-        //         University of Alberta pursing a Specialized Computer Science
-        //         Degree.
-        //     </h1>
-        //     <h1 className="mainText">
-        //         In my spare time, I play and coach badminton. I have played
-        //         badminton for 12+ years and enjoy giving back to the
-        //         community. Since I was once in their shoes, I believe that
-        //         having a mentor and a friend working towards a common goal
-        //         is a special connection that can be built though sports.
-        //     </h1>
-        //     <h1>
-        //         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        //         Voluptatum nemo numquam impedit placeat aut at cumque
-        //         pariatur sint illo eius minus veniam, vitae voluptates ut
-        //         eum necessitatibus nostrum molestiae illum.
-        //     </h1>
-        // </div> */}
     );
 }
 
