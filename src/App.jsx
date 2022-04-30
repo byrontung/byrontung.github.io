154272a30282c4596e0b2810fa98f4ee8c2b5286
byrontung <btung1@ualberta.ca>
2022-04-30 17:34:09 -0600

    formatted with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ import Contact from "./component/contact/Contact";
 import { Container } from "react-bootstrap";
 
 function App() {
-    return (
-        <Container fluid className="p-0">
-            {/* This is causing: react_devtools_backend.js:3973 Warning: Using UNSAFE_componentWillMount in strict mode is not recommended and may indicate bugs in your code. */}
-            <Helmet>
-                <title>Byron Tung</title>
-            </Helmet>
-            <Welcome />
-            <About />
-            <Projects />
-            <Contact />
-        </Container>
-    );
+  return (
+    <Container fluid className="p-0">
+      {/* This is causing: react_devtools_backend.js:3973 Warning: Using UNSAFE_componentWillMount in strict mode is not recommended and may indicate bugs in your code. */}
+      <Helmet>
+        <title>Byron Tung</title>
+      </Helmet>
+      <Welcome />
+      <About />
+      <Projects />
+      <Contact />
+    </Container>
+  );
 }
 
 export default App;
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -4,83 +4,69 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 import resume from "../../img/ByronTungResume2022Fall.pdf";
-import "./Contact.css";
 import { Col, Row } from "react-bootstrap";
 
 function Contact() {
-    const styles = {
-        backgroundLeft: { backgroundColor: "#DFDFC8" },
-        backgroundRight: { backgroundColor: "#b19cd9" },
-    };
-    return (
-        <Row
-            id="contact"
-            className="d-flex position-relative min-vh-100 w-100 p-0 m-0"
-        >
-            <Col
-                id="contact_left"
-                className="position-sticky flex-grow-1 flex-shrink-0 d-flex align-items-center h-auto p-0"
-                style={styles.backgroundLeft}
+  const styles = {
+    backgroundLeft: { backgroundColor: "#DFDFC8" },
+    backgroundRight: { backgroundColor: "#b19cd9" },
+  };
+  return (
+    <Row
+      id="contact"
+      className="d-flex position-relative min-vh-100 w-100 p-0 m-0"
+    >
+      <Col
+        id="contact_left"
+        className="position-sticky flex-grow-1 flex-shrink-0 d-flex align-items-center h-auto p-0"
+        style={styles.backgroundLeft}
+      >
+        <div className="position-sticky flex-column d-flex justify-content-center top-0 bottom-0 align-items-center w-100 vh-100">
+          <h1 className="title fw-bold">Contact</h1>
+          <div
+            className="d-flex flex-row position-absolute"
+            style={{ bottom: "3vh" }}
+          >
+            <BottomNagivationBar />
+          </div>
+        </div>
+      </Col>
+      <Col
+        id="contact_right"
+        className="d-flex h-100 flex-grow-1 flex-shrink-0 flex-column align-items-center position-relative p-0"
+        style={styles.backgroundRight}
+      >
+        <div className="vh-100 w-100 d-flex flex-row justify-content-evenly align-items-center">
+          <div className="d-flex flex-column align-items-center">
+            <a
+              href="https://www.linkedin.com/in/byron-tung/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-                <div className="position-sticky flex-column d-flex justify-content-center top-0 bottom-0 align-items-center w-100 vh-100">
-                    <h1 className="title">Contact</h1>
-                    <div className="main">
-                        <BottomNagivationBar />
-                    </div>
-                </div>
-            </Col>
-            <Col
-                id="contact_right"
-                className="d-flex h-100 flex-grow-1 flex-shrink-0 flex-column align-items-center position-relative p-0"
-                style={styles.backgroundRight}
+              <FontAwesomeIcon icon={faLinkedin} fontSize={100} color="black" />
+            </a>
+            <h1>LinkedIn</h1>
+          </div>
+          <div className="d-flex flex-column align-items-center">
+            <a
+              href="https://github.com/byrontung"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-                <div className="vh-100 w-100 d-flex flex-row justify-content-evenly align-items-center">
-                    <div className="d-flex flex-column align-items-center">
-                        <a
-                            href="https://www.linkedin.com/in/byron-tung/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FontAwesomeIcon
-                                icon={faLinkedin}
-                                fontSize={100}
-                                color="black"
-                            />
-                        </a>
-                        <h1>LinkedIn</h1>
-                    </div>
-                    <div className="d-flex flex-column align-items-center">
-                        <a
-                            href="https://github.com/byrontung"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FontAwesomeIcon
-                                icon={faGithub}
-                                fontSize={100}
-                                color="black"
-                            />
-                        </a>
-                        <h1>Github</h1>
-                    </div>
-                    <div className="d-flex flex-column align-items-center">
-                        <a
-                            href={resume}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FontAwesomeIcon
-                                icon={faFile}
-                                fontSize={100}
-                                color="black"
-                            />
-                        </a>
-                        <h1>Resume</h1>
-                    </div>
-                </div>
-            </Col>
-        </Row>
-    );
+              <FontAwesomeIcon icon={faGithub} fontSize={100} color="black" />
+            </a>
+            <h1>Github</h1>
+          </div>
+          <div className="d-flex flex-column align-items-center">
+            <a href={resume} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFile} fontSize={100} color="black" />
+            </a>
+            <h1>Resume</h1>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
 }
 
 export default Contact;
